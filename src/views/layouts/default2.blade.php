b06987d7985ff35ace635b357fac082e2ca7ab16
kadirarli <kadirarli@sirketing.com>
2015-12-08 11:15:13 +0200

    ckeditor bug is fixed.

--- src/views/layouts/default2.blade.php
+++ src/views/layouts/default2.blade.php
@@ -11,6 +11,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 3.3.2 -->
     <link href="{{ asset ("packages/frozennode/administrator/bower_components/AdminLTE/bootstrap/css/bootstrap.min.css") }}" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.ckeditor.com/4.4.3/standard/ckeditor.js"></script>
     <!-- Font Awesome Icons -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
     <!-- Ionicons -->
